1c1a74d04e30cf0634708563effbbc93841c627c
Igor Dragun <dragun.msc@gmail.com>
2022-11-25 09:19:56 +0300

    fix migration

--- migrations/20221121120000_tablemessages.up.sql
+++ migrations/20221121120000_tablemessages.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS messages (
     sender varchar(32) NOT NULL,
     receiver varchar(32) NOT NULL,
-    msg varchat(256) NOT NULL,
+    msg varchar(256) NOT NULL,
     CONSTRAINT fk_sender
         FOREIGN KEY(sender)
             REFERENCES users(name),
