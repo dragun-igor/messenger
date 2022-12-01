09308226bf74e693e00e81c1d02e8ac80a59d89a
Dragun Igor <dragun.msc@gmail.com>
2022-12-02 00:01:29 +0300

    fix migration

--- migrations/20221120120000_tableusers.up.sql
+++ migrations/20221120120000_tableusers.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     login varchar(32) NOT NULL UNIQUE,
     name varchar(32) NOT NULL UNIQUE,
-    password varchar(32) NOT NULL
+    password varchar(128) NOT NULL
 );
